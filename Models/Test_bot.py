e0a622fb3bae7f99cae05723d3c4dfba9cf7bef0
Shishir-Kc <shishir.khatri.kc@gmail.com>
2025-10-16 19:38:15 +0545

    added some testing models

--- Models/Test_bot.py
+++ Models/Test_bot.py
@@ -1,30 +1,56 @@
-import os
-from openai import OpenAI
-from dotenv import load_dotenv
-load_dotenv()
+# import os
+# from openai import OpenAI
+# from dotenv import load_dotenv
+# load_dotenv()
 
-client = OpenAI(
-    base_url="https://router.huggingface.co/v1",
-    api_key=os.environ.get("HF_TOKEN"),
-)
+# client = OpenAI(
+#     base_url="https://router.huggingface.co/v1",
+#     api_key=os.environ.get("HF_TOKEN"),
+# )
 
 
 
-completion = client.chat.completions.create(
-    model="openai/gpt-oss-120b:fireworks-ai",
-    messages=[
-        {
-            "role": "user",
-            "content": "What is the weather like in Paris today?"
-        }
+# completion = client.chat.completions.create(
+#     model="openai/gpt-oss-120b:fireworks-ai",
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "What is the weather like in Paris today?"
+#         }
     
-    ],
-)
+#     ],
+# )
 
 
 
 
-reasoning = completion.choices[0].message.content
-print("Content:\n", reasoning)
+# reasoning = completion.choices[0].message.content
+# print("Content:\n", reasoning)
 
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+api_key = os.environ.get("OR_API_KEY")
+import requests
+
+url = "https://openrouter.ai/api/v1/chat/completions"
+
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json",
+}
+while True:
+    data = {
+     "model": "cognitivecomputations/dolphin-mistral-24b-venice-edition:free",
+     "messages": [
+         {"role": "system", "content": "You are a assistant "},
+         {"role": "user", "content": input()}
+     ]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    print('===========================================================')
+    # print(response.json()["choices"][0]["message"]["content"])
+    print('===========================================================')
+    print(response.json())
